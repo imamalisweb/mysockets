68bcc101a6ea0d30478786b9e750bcaeed7a65d8
imamalisweb <imamalisweb@gmail.com>
2016-09-16 23:05:13 +0300

    temp sock class

---
+++ pythonsock.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+	print "class constructor......"
+
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data 
+                    response = data
+		    client.send('you are the client')
+		    #client.send(client)
+		    #client.send('you have address')	
+		    #client.send(address)
+                    client.send( response )
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    port_num = input("in which port ? ")
+    ThreadedServer('',port_num).listen()
+
